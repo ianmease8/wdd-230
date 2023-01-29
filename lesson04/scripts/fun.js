8af0a1a43775d1c8ff8b14e4ee29eb8d8453cca7
imease8@gmail.com <imease8@gmail.com>
2023-01-28 20:48:19 -0700

    lots of things

---
+++ lesson04/scripts/fun.js
@@ -0,0 +1,29 @@
+const button = document.querySelector('.btn');
+
+// function show(){
+//     button.classList.add('newstyle');
+// }
+
+
+button.addEventListener('click', () => {
+    button.classList.toggle('newstyle');
+});
+
+
+// hamburger button js 
+const mainnav = document.querySelector('.navigation');
+const hambutton = document.querySelector('.ham');
+
+hambutton.addEventListener('click', () =>{
+    mainnav.classList.toggle('responsive');
+});
+
+let date = new Date();
+let year = date.getFullYear();
+
+
+document.querySelector('#feets').innerHTML ='&copy;'+ year; 
+
+let currentdate = document.lastModified;
+
+document.querySelector('#modified').innerHTML = 'Last updated: ' + currentdate;
