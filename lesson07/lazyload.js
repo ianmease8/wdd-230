abceadaff4115f0d1c3ef282c84cbd5e4990eb12
imease8@gmail.com <imease8@gmail.com>
2023-02-16 17:29:14 -0700

    big changes

--- lesson07/lazyload.js
+++ lesson07/lazyload.js
@@ -1,9 +1,31 @@
-const images = document.querySelector('img');
+const images = document.querySelectorAll('[data-src]');
+
+const options = {threshold: .5};
+
+function preloadImages(img) {
+    const source = img.getAttribrute("data-src")
+    if (!source){
+        return;
+    }
+    img.src = source;
+}
 
 const io = new IntersectionObserver (
-    (entries, io) => {
-        console.log(entries);
-        console.log(io);
-    }
+    (entries) => {
+        entries.forEach(entry=> {
+            if(!entries.isIntersecting){
+                console.log(entries);
+            }
+            else{
+                preloadImages(entry.target)
+            }
+        })
+        // console.log(entries);
+        // console.log(io);
+    },
+    options
 );
-io.observe(images)
+// io.observe(images)
+images.forEach(img=> {
+    io.observe(img)
+})
