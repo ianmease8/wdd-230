7f599b0a36c74dcc8b166006893ba39458dce665
imease8@gmail.com <imease8@gmail.com>
2023-02-16 17:42:07 -0700

    java please bro

--- lesson07/lazyload.js
+++ lesson07/lazyload.js
@@ -1,31 +1,34 @@
 const images = document.querySelectorAll('[data-src]');
 
-const options = {threshold: .5};
+const options = {
+    threshold: .5};
 
-function preloadImages(img) {
-    const source = img.getAttribrute("data-src")
-    if (!source){
+function preloadImage(img){
+    const source = img.getAttribute('data-src');
+    if(!source){
         return;
     }
     img.src = source;
 }
 
-const io = new IntersectionObserver (
-    (entries) => {
-        entries.forEach(entry=> {
-            if(!entries.isIntersecting){
-                console.log(entries);
+const io = new IntersectionObserver(
+    (entries)=>{
+        entries.forEach(entry => {
+            if(!entry.isIntersecting) {
+                return;
+            } else{
+                preloadImage(entry.target);
+                io.unobserve(entry.target);
             }
-            else{
-                preloadImages(entry.target)
-            }
+            
+            console.log(entries);
         })
-        // console.log(entries);
-        // console.log(io);
+
+
     },
     options
 );
-// io.observe(images)
-images.forEach(img=> {
-    io.observe(img)
+
+images.forEach(img => {
+    io.observe(img);
 })
