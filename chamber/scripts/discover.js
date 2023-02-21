564f0b870ae6c105a4f8371f2a409e9b30bfbeee
imease8@gmail.com <imease8@gmail.com>
2023-02-20 22:07:29 -0700

    work would ya

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,9 +1,11 @@
 if(!localStorage.getItem('lastVisit')){
     localStorage.setItem('lastVisit',Date.now())
-    document.querySelector('#diff').innerHTML = "Welcome"
+    document.querySelector('#diff').innerHTML = "Welcome";
 }
 else{
     let currentdate=Date.now
-    let lastVisit = localStorage.getItem()
+    let lastVisit = localStorage.getItem('LastVisit')
     let diff = (currentdate-lastVisit)/1000/60/60/24
-};
+    difference = Math.round(difference)
+    document.querySelector("#diff").innerHTML = difference;
+}
