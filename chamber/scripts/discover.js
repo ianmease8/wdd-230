576a7abd5957171174f3a5a0eab9da0e583320cd
imease8@gmail.com <imease8@gmail.com>
2023-02-21 13:22:26 -0700

    work would you

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,11 +1,11 @@
 if(!localStorage.getItem('lastVisit')){
-    localStorage.setItem('lastVisit',Date.now())
+    localStorage.setItem('lastVisit',Date.now());
     document.querySelector('#diff').innerHTML = "Welcome";
 }
 else{
-    let currentdate=Date.now
-    let lastVisit = localStorage.getItem('LastVisit')
-    let diff = (currentdate-lastVisit)/1000/60/60/24
-    diff = Math.round(diff)
+    let currentdate=Date.now();
+    let lastVisit = localStorage.getItem('LastVisit');
+    let diff = (currentdate-lastVisit)/1000/60/60/24;
+    diff = Math.round(diff);
     document.querySelector("#diff").innerHTML = diff;
 }
