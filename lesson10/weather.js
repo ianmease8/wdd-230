fea6a7b244a831140d7e200507e27085c11f6203
imease8@gmail.com <imease8@gmail.com>
2023-03-08 12:49:56 -0700

    weathers

---
+++ lesson10/weather.js
@@ -0,0 +1,14 @@
+const apiURL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/wakefield%20rhode%20island?unitGroup=us&key=2P7SUHYC4RMT9L5759A8NEBTR&contentType=json";
+const getWeather = async () => {
+    const response = await fetch(apiURL);
+    const data = await response.json();
+    console.log(data);
+    let t = data.currentConditions.temp;
+    document.querySelector('#t').textContent = t.toFixed(2);
+    let image = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${data.currentConditions.icon}.svg`;
+    document.querySelector('#ws').textContent = data.currentConditions.windspeed;
+    document.querySelector('#condition').textContent =  data.currentConditions.conditions;
+    document.querySelector('#weather_icon').src = image;
+    document.querySelector('#weather_icon').alt= data.currentConditions.conditions + ' icon';
+  };
+getWeather();
