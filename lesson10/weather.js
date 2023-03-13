04251d9a3e706b4b47d68a9016c4d4dcb00a5779
imease8@gmail.com <imease8@gmail.com>
2023-03-13 13:18:28 -0600

    lots of changes

--- lesson10/weather.js
+++ lesson10/weather.js
@@ -20,7 +20,7 @@ const windspeed = document.querySelector('#ws').textContent;
 
 let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
 
-if (temp <=50 && windspeed > 3){
-    let windchill = document.querySelector('#wc').textContent;
+if (temp <= 50 && windspeed >= 3){
+    const windchill = document.querySelector('#wc');
     windchill.textContent = chill;
 };
