d294eb734ddd6e3ba085b957db231c22c4f9acec
imease8@gmail.com <imease8@gmail.com>
2023-03-18 12:03:19 -0600

    maybe

--- lesson10/weather.js
+++ lesson10/weather.js
@@ -10,17 +10,20 @@ const getWeather = async () => {
     document.querySelector('#condition').textContent =  data.currentConditions.conditions;
     document.querySelector('#weather_icon').src = image;
     document.querySelector('#weather_icon').alt= data.currentConditions.conditions + ' icon';
+
+
+
+    const temp = document.querySelector('#t').textContent;
+    const windspeed = document.querySelector('#ws').textContent;
+
+
+
+    let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
+
+    if (temp <= 50 && windspeed >= 3){
+        const windchill = document.querySelector('#wc');
+        windchill.textContent = chill;
+    };
   };
 getWeather();
 //wind chill calculation //
-const temp = document.querySelector('#t').textContent;
-const windspeed = document.querySelector('#ws').textContent;
-
-
-
-let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
-
-if (temp <= 50 && windspeed >= 3){
-    const windchill = document.querySelector('#wc');
-    windchill.textContent = chill;
-};
