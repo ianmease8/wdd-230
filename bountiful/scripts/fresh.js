2f2f5876767832c3971ef5e78cf14e8d6194dfa0
imease8@gmail.com <imease8@gmail.com>
2023-04-05 01:03:49 -0600

    hmm

--- bountiful/scripts/fresh.js
+++ bountiful/scripts/fresh.js
@@ -43,7 +43,7 @@ async function getProphetData1() {
     const data = await response.json();
     return data;
 }
-let data = getProphetData1();
+let data = await getProphetData1();
 console.table(data);
 async function getProphetData() {
     const response = await fetch(url);
