992af0e9c5ee67030ea7b5ac7c6a7883f5ab36e7
imease8@gmail.com <imease8@gmail.com>
2023-04-05 00:09:48 -0600

    maybe

--- bountiful/scripts/fresh.js
+++ bountiful/scripts/fresh.js
@@ -23,16 +23,65 @@ async function getProphetData() {
     f4.textContent = fourthfruit.name;
     f5.textContent = fifthfruit.name;
 
-
-
 }
 
 getProphetData();
 
 
+const formbutton = document.querySelector('.formbutton');
+const clearbutton = document.querySelector('.clearbutton');
+const formers = document.querySelector(".form");
+const visible = document.querySelector(".visible");
+
+const firstname = document.querySelector('.firstname');
+const email = document.querySelector('.email');
+const number = document.querySelector('.number');
+const fruity1 = document.querySelector('.fruity1');
+const fruity2 = document.querySelector('.fruity2');
+const fruity3 = document.querySelector('.fruity3');
+const fruity4 = document.querySelector('.fruity4');
+const fruity5 = document.querySelector('.fruity5');
+const info = document.querySelector('.info');
+const carbs = document.querySelector('.carbs');
+const protein = document.querySelector('.protein');
+const fat = document.querySelector('.fat');
+const sugar = document.querySelector('.sugar');
+const calories = document.querySelector('.calories');
+
+
+cardbutton.addEventListener('click', () =>{
+    const firstnameu = document.querySelector('input[name="fname"]');
+    const emailu = document.querySelector('input[name="email"]');
+    const numberu = document.querySelector('input[name="phone"]');
+    const fruity1u = document.querySelector('input[name="fruit1"]');
+    const fruity2u = document.querySelector('input[name="fruit2"]');
+    const fruity3u = document.querySelector('input[name="fruit3"]');
+    const fruity4u = document.querySelector('input[name="fruit4"]');
+    const fruity5u = document.querySelector('input[name="fruit5"]');
+    const infou1 = document.querySelector('.tainfo');
+    const infou2 = infou1.value;
+
+    firstname.textContent = firstnameu.value;
+    email.textContent = emailu.value;
+    number.textContent = numberu.value;
+    fruity1.textContent = fruity1u.value;
+    fruity2.textContent = fruity2u.value;
+    fruity3.textContent = fruity3u.value;
+    fruity4.textContent = fruity4u.value;
+    fruity5.textContent = fruity5u.value;
 
 
 
+
+
+
+
+
+
+    visible.style.display = "flex";
+    formers.style.display = "none";
+  
+  });
 
 
 
